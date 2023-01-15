ee2036ece176724dd9508ad76f5ea1820b1e1a91
Juan Sierra <juan.sierra@meltstudio.co>
2023-01-15 03:16:07 -0500

    chore: progress 14/01

--- data-structures/linkedLists/linkedList.c
+++ data-structures/linkedLists/linkedList.c
@@ -16,6 +16,28 @@ struct Node
 
 struct LinkedList myLinkedList = {NULL, NULL};
 
+int size();
+bool empty();
+int value_at(int index);
+void push_front(int value);
+void print();
+
+int main()
+{
+  struct Node head = {NULL, NULL, 1};
+  myLinkedList.head = &head;
+  myLinkedList.tail = &head;
+  struct Node newOne = {NULL, &head, 2};
+  head.next = &newOne;
+
+  printf("%d Number of nodes in the linked list\n", size());
+  printf(empty() ? "Is empty\n" : "Not empty\n");
+  printf("%d is the value at index 0\n", value_at(1));
+  push_front(5);
+  print();
+
+  return 0;
+}
 int size()
 {
   if (!myLinkedList.head)
@@ -53,16 +75,21 @@ int value_at(int index)
   return current->value;
 }
 
-int main()
+void push_front(int value)
 {
-  struct Node head = {NULL, NULL, 1};
-  myLinkedList.head = &head;
-  myLinkedList.tail = &head;
-  struct Node newOne = {NULL, &head, 2};
-  head.next = &newOne;
+  struct Node newNode = {myLinkedList.head, NULL, value};
+  myLinkedList.head->before = &newNode;
+  myLinkedList.head = &newNode;
+}
 
-  printf("%d Number of nodes in the linked list\n", size());
-  printf(empty() ? "Is empty\n" : "Not empty\n");
-  printf("%d is the value at index 0\n",value_at(1) );
-  return 0;
+void print()
+{
+  struct Node *current = myLinkedList.head;
+  while (current->next)
+  {
+    current = current->next;
+    printf("Node value: %d\n", current->before->value);
+  }
+  printf("Node value: %d\n", current->value);
+  return;
 }
